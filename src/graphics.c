d1479d21dc97a433fedf762f48063d321971bb95
Tyler Wight <tylermwight@gmail.com>
2024-10-02 21:06:43 -0600

    more test

--- src/graphics.c
+++ src/graphics.c
@@ -238,7 +238,9 @@ GLFWwindow* setup_opengl(int resolution_x, int resolution_y, void (*key_callback
 
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
 
         window = glfwCreateWindow(resolution_x, resolution_y, "Snek", NULL, NULL);
         if (!window){
